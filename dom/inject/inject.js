5d353bf9f11b5352503861762b3bdee5a7a6e478
NicholsZane13 <58497365+NicholsZane13@users.noreply.github.com>
2021-01-28 23:05:03 -0600

    Add files via upload

--- dom/inject/inject.js
+++ dom/inject/inject.js
@@ -1,4 +1,9 @@
-/*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html.  You are not allowed to simply set the innerHTML of the <div> to the entire html document. 
+/*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript 
+DOM manipulation to inject the html into the inject id element of inject.html.  You are not allowed to simply set the innerHTML of the <div> to the entire html document. 
 */
 
-
+(function html() {
+    let scrt_var = 10;
+    let strLink = "https://codepen.io/ayunas-the-scripter/pen/WNQgpqB" + scrt_var;
+    document.getElementById("inject").setAttribute("id",strLink);
+})();
