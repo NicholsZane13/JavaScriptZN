832fe2cd8702e0011d6bf48284e084b9f5e21243
NicholsZane13 <58497365+NicholsZane13@users.noreply.github.com>
2021-01-28 23:03:16 -0600

    Add files via upload

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,20 @@
-console.log("time to start the timer!");
 
+console.log("time to start the timer!");
+let timeElapsed = 0;
+let myTimer = 0;
+
+function start() {
+    myTimer = setInterval(function(){
+        timeElapsed += 1;
+         document.getElementById("time").innerText = timeElapsed;
+    }, 1000) ;
+
+}
+function stop() {
+    clearInterval(myTimer);
+}
+function reset() {
+   timeElapsed = 0;
+   clearInterval(myTimer);
+   document.getElementById("time").innerHTML = timeElapsed;
+}
